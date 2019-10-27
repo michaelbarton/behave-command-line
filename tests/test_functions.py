269479f3d7b7138072d31e51ee6d2c25460ce3ee
Michael Barton <mail@michaelbarton.me.uk>
2019-10-26 20:35:10 -0700

    Stub out units tests for behave CLI functions

---
+++ tests/test_functions.py
@@ -0,0 +1,82 @@
+def test_get_stream() -> None:
+    pass
+
+
+def test_create_the_file() -> None:
+    pass
+
+
+def test_delete_the_file() -> None:
+    pass
+
+
+def test_create_the_file_with() -> None:
+    pass
+
+
+def test_create_the_delimited_file_with() -> None:
+    pass
+
+
+def test_gzip_the_file() -> None:
+    pass
+
+
+def test_create_the_directory() -> None:
+    pass
+
+
+def test_run_the_command_with_args() -> None:
+    pass
+
+
+def test_run_the_command() -> None:
+    pass
+
+
+def test_the_stream_should_contain() -> None:
+    pass
+
+
+def test_the_stream_should_contain_text() -> None:
+    pass
+
+
+def test_the_stream_should_equal() -> None:
+    pass
+
+
+def test_the_exit_code_should_be() -> None:
+    pass
+
+
+def test_the_io_type_was_created() -> None:
+    pass
+
+
+def test_the_io_type_should_not_exist() -> None:
+    pass
+
+
+def test_the_files_should_exist() -> None:
+    pass
+
+
+def test_the_file_should_exist_with() -> None:
+    pass
+
+
+def test_the_file_should_contain() -> None:
+    pass
+
+
+def test_the_file_should_include() -> None:
+    pass
+
+
+def test_the_file_should_have_permissions() -> None:
+    pass
+
+
+def test_the_stream_should_be_empty() -> None:
+    pass
